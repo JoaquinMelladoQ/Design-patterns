f778cc0624618e63d7c711e1b511bd01aa73cbbe
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-18 22:43:34 -0300

    declared class and adjusting an img to readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Design-patterns
 
++ Firs steps to execute it with node: 
+![image info](./assets/constructor-instructions.png)
---
+++ creational/constructor.js
@@ -0,0 +1,11 @@
+class MiClase {
+    constructor() {
+        this.propiedad = 1
+        this.metodo = () => {
+            // I am a method
+        }
+    }
+}
+
+const instancia = new MiClase()
+console.log(instancia)
