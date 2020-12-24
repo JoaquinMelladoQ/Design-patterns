1ace9e528ef0a07466062e7a134cec5b6a7ce798
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-24 17:53:15 -0300

    second step

--- functional/composition-two.js
+++ functional/composition-two.js
@@ -6,13 +6,13 @@ const users = [
 ]
 
 const head = xs => xs[0]
+const formateo = x => ({
+    nombreCompleto: `${x.nombre} ${x.apellido}`,
+    edad: x.edad,
+})
 
 const traePrimerInfante = data => {
-    const primerInfante = head(data.filter(x => x.edad < 2))
-    const infante = {
-        nombreCompleto: `${primerInfante.nombre} ${primerInfante.apellido}`,
-        edad: primerInfante.edad,
-    }
+    const primerInfante = formateo(head(data.filter(x => x.edad < 2)))
 
     return `${infante.nombreCompleto} tiene ${infante.edad} year()`
 }
