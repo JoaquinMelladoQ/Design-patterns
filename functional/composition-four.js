d633af8fca81e77d711975500e20b842ef535907
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-24 18:38:42 -0300

    refactoring same code buth with pipe this time

---
+++ functional/composition-four.js
@@ -0,0 +1,26 @@
+const users = [
+    { edad: 17, nombre: 'Nicolas', apellido: 'Soto' }
+    { edad: 18, nombre: 'Chanchito', apellido: 'Feliz' }
+    { edad: 12, nombre: 'Loreto', apellido: 'Fernandez' }
+    { edad: 1, nombre: 'Sofia', apellido: 'Zapata' }
+]
+
+const compose = (...fns) => x => fns.reduceRight((y, f) => f(y), x)
+const pipe = (...fns) => x => fns.reduce((y, f) => f(y), x)
+const filter = f = xs => xs.filter(f)
+const head = xs => xs[0]
+const formateo = x => ({
+    nombreCompleto: `${x.nombre} ${x.apellido}`,
+    edad: x.edad,
+})
+const string = x => `${x.nombreCompleto} tiene ${x.edad} year()`
+
+// all this functions above should be place in another file for good practice
+
+const traePrimerInfante = pipe(
+    filter(x => x.edad < 2),
+    head,
+    formateo,
+    string,
+)
+    
